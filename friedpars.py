97ef9ac0f452922eee166ac6d088eacbfb94109d
Andreas Hellwig <marvin@avatar.(none)>
2011-05-31 14:59:30 +0200

    still trying to get a real Parser working...

--- friedpars.py
+++ friedpars.py
@@ -2,6 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import codecs
+from HTMLParser import HTMLParser
+
+class HTMLParser(HTMLParser):
+
+  def handle_data(self, text):
+    self.captionlist = []
+    for i in range(1,7):
+      tagtext = repr(self.get_starttag_text())
+      snippet =  sanitize_snippet(text, False)
+      if tagtext.count("H1") == 1 and snippet !=  '':
+        caption = snippet
+        self.captionlist.append(caption)
+   
 
 lesson_nr = 15
 
@@ -11,11 +24,14 @@ OUTFILE = PATH+"lek"+str(lesson_nr)+"_LL.html"
 SRCFILE = PATH+"lek"+str(lesson_nr)+".html"
 XMLFILE = PATH+"lek"+str(lesson_nr)+".xml"
 
+#HTML Templates and Colorcodes to use in the write_html function
 HTMLRES_Path = "HTMLRES"
 
 colordic = {"Title":"#000066", "Text": "#0099cc"}
 colordic["Vocs"] = "#99ffcc"
 colordic["Grammar"] ="#ff6699"
+
+#reads the Libre-Office-generated html and returns the whole text in utf-8
 
 def read_html(html_file):
   input_file = codecs.open(html_file, 'r', "utf-8")
@@ -25,6 +41,9 @@ def read_html(html_file):
   input_file.close()
   return text
 
+          
+#Clears a line of HTML of some annoying symbols.
+
 def sanitize_snippet(snippet, onlytabs):
   if not onlytabs:
     snippet = snippet.replace('\n',' ')
@@ -33,15 +52,20 @@ def sanitize_snippet(snippet, onlytabs):
   snippet = snippet.replace('\t','')
   return snippet
 
-def capture_caption(html_tag, snippet, captionlist):
+#Checks if a snippet is a Heading by searching its HTML_Tag for H#
+#stores it in a given list and returns the caption
+
+def capture_caption(html_tag, snippet):
   for i in xrange(10):
     snippet = sanitize_snippet(snippet, False)
     if html_tag.count("H"+str(i)) == 1 and snippet!='':
-      captionlist.append(snippet)
+      caption = snippet
+      return caption
     elif html_tag.count("H"+str(i)) > 1:
       pass
       #print "SOMETHINGS WRONG!!!! with the Caption tags. Search for \"H"+str(i)+"\" "
        ##Da müssen noch die Style Vorgaben für Hx ausgeblendet werden...
+
 
 def snippetize(text):
   # Seperating the html_tags and the actual text. Then recreate the whole text without formatting
@@ -50,14 +74,32 @@ def snippetize(text):
   captionlist = []
   htmltaglist = []
   snippetlist = []
+  
+  snipseclist = []
+  
+  snipsecdict= {}
+  section = "Header"
+  snipsecdict[section] = ''
+
+  
   for i in xrange(text.count("<")+1):
-    htmltag = text.split("<")[i].split(">")[0]
+    htmltag = text.split("<")[i].split(">")[0] #returns the htmltag
     htmltaglist.append(htmltag)
     
-    snippet = text.split(">")[i].split("<")[0]
+    snippet = text.split(">")[i].split("<")[0] #retruns the text between the htmltags
+
+    ###ABER ES GIBT ZWEI TAG PRO SNIPPET!!!
+    
+    if capture_caption(htmltag, snippet): # if a new caption is found
+      section=capture_caption(htmltag, snippet)
+      captionlist.append(section)
+      snipsecdict[section] = ''
+    
     snippetlist.append(snippet)
-    capture_caption(htmltag, snippet, captionlist)
+    snipseclist.append((snippet, section)) #to have a connection between lines and their section
     snippettext = snippettext + snippet
+    
+    snipsecdict[section] += snippet
     
   if snippettext.count("#")%(2) != 0:
     print "Odd number of '#'-Symbols. That's not good!"
@@ -65,6 +107,8 @@ def snippetize(text):
   returndict = {"sniptext": snippettext, "sniplist": snippetlist,}
   returndict["captionlist"] = captionlist
   returndict["taglist"] = htmltaglist
+  returndict["snipseclist"] = snipseclist
+  returndict["snipsecdict"] = snipsecdict
 
   return returndict
      
@@ -88,25 +132,6 @@ def dehash(sniptext):
       
   return {"outsnips": outsnips, "insnips":insnips}
 
-### THIS WAS A TRY TO CONSERVE THE LIBREOFFICE GENERATED HTML CODE. NOW THE HTML IS COMPLETELY
-### CREATED ANEW
-
-##Nur noch Zeilenumbrüche scheinen das Problem zu sein!
-
-#### -> Problem: Die "#"'en sind in vielen Fällen vom Text durch Tags getrennt.
-#### -> Lösungsidee: Snippets und Text einzeln wieder zusammensetzen, wie ich's oben auch auseinandergenommen hab.
-
-#def replacehash(snippetlist, taglist):
-#for i in xrange(len(snippetlist)):
-#a = 1
-#snippetchain_forward = snippetlist[i]
-#snippetchain_backward = snippetlist[i]
-#while ((snippetchain_forward).count("#") not in [0,2]) and a <= 3:
-##print snippetchain_forward, str(snippetchain_forward.count("#"))
-#snippetchain_forward = snippetchain_forward + snippetlist[i+a]
-#a += 1
-###########################
-
 def rebuild(dehashed_dict):
   ins = dehashed_dict["insnips"]
   outs = dehashed_dict["outsnips"]
@@ -116,12 +141,9 @@ def rebuild(dehashed_dict):
     new_text = new_text + "###_LL_LABEL_LESSON"+str(lesson_nr)+"_ITEM"+str(i)+"###"
   return new_text
 
-def write_html(content):
-  
-  #CONTENTFILE = PATH+"lek15_LLroh.html"
-  #OUTFILE = PATH+"lek15_LL.html"
-  
-  outfile = codecs.open(OUTFILE, 'w', "utf-8")
+def htmlize(content):
+  #translates the content from utf-8 to html
+  #There's probably a better way...
   
   content = content.replace("\n", "<br>")
   content = content.replace(unichr(252), "&uuml;")
@@ -134,21 +156,58 @@ def write_html(content):
   content = content.replace(unichr(8222), "\"")
   content = content.replace(unichr(8221), "\"")
   content = content.replace(unichr(8220), "\"")
-
+  
   content = content.replace(unichr(8211), "-")
   content = content.replace(unichr(8212), "-")
 
+  return content
+
+def write_html(snipdic):
+  #TODO: Hier noch die snapseclist benutzen für die
+  #automatische Erstellung von verschiedenen Abschnitten
+
+  snipsecdict = snipdic["snipsecdict"]
+  captionlist = snipdic["captionlist"]
+  outfile = codecs.open(OUTFILE, 'w', "utf-8")
+
+
+#Write the Header
   #Read the lines of the different templates
-  for vorlage in ["Head.txt", "Body.txt", "Foot.txt"]:
+  for vorlage in ["Head.txt"]:
     vorfile = codecs.open(HTMLRES_Path + "/"+vorlage, 'r', "utf-8")
     lines = vorfile.readlines()
     vorfile.close()
     #And write them in the new file, replacing ###TEXT### with the content
     for line in lines:
-      line = line.replace("###TEXT###", content)
       line = line.replace("###COLOR###", colordic["Grammar"])
-
       outfile.write(line)
+
+
+#write the content
+  for section in captionlist:
+    content = snipsecdict[section]
+    dehashed_dict = dehash(content)
+    content = rebuild(dehashed_dict)
+    content = htmlize(content)
+
+    for vorlage in ["Body.txt"]:
+      vorfile = codecs.open(HTMLRES_Path + "/"+vorlage, 'r', "utf-8")
+      lines = vorfile.readlines()
+      vorfile.close()
+        #And write them in the new file, replacing ###TEXT### with the content
+      for line in lines:
+        line = line.replace("###TEXT###", content)
+        line = line.replace("###COLOR###", colordic["Grammar"])
+        outfile.write(line)
+
+#Write the Footer       
+  for vorlage in ["Foot.txt"]:
+    vorfile = codecs.open(HTMLRES_Path + "/"+vorlage, 'r', "utf-8")
+    lines = vorfile.readlines()
+    vorfile.close()
+    for line in lines:
+      outfile.write(line)
+      
   outfile.close()
 
 
@@ -161,8 +220,14 @@ def write_xml(sniplist):
   output_file.close()
 
 
+    
+    
+parser = HTMLParser()
 
 snipdic = snippetize(read_html(SRCFILE))
-dehashed_dict = dehash(snipdic["sniptext"])
-write_html(rebuild(dehashed_dict))
+parser.feed(read_html(SRCFILE))
+
+#dehashed_dict = dehash(snipdic["sniptext"])
+#write_html(rebuild(dehashed_dict))
+write_html(snipdic)
 write_xml(snipdic["sniplist"])
